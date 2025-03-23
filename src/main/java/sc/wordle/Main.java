adecfba966ad87df1b8e3139f1240b53b4c7a6f3
Shawn_Chung <c_shawn710@yahoo.com>
2025-03-22 20:30:54 -0700

    play again loop

--- src/main/java/sc/wordle/Main.java
+++ src/main/java/sc/wordle/Main.java
@@ -7,11 +7,33 @@ public class Main {
         WordManager wordManager = new WordManager();
         GuessEvaluator guessEvaluator = new GuessEvaluator();
         Scanner scanner = new Scanner(System.in);
-        String secretWord = wordManager.selectRandomWord();
 
-        //Create and start the game
-        WordleGame game = new WordleGame(guessEvaluator, scanner, secretWord);
-        game.start();
+        boolean playAgain = true;
+        while (playAgain) {
+            String secretWord = wordManager.selectRandomWord();
+
+            //Create and start the game
+            WordleGame game = new WordleGame(guessEvaluator, scanner, secretWord);
+            game.start();
+
+            while (true) {
+                System.out.println("Play again? (Y / N)");
+                String response = scanner.nextLine();
+
+                if (response.equalsIgnoreCase("Y")) {
+                    playAgain = true;
+                    break;
+                } else if (response.equalsIgnoreCase("N")) {
+                    playAgain = false;
+                    break;
+                } else {
+                    System.out.println("Invalid input.");
+                }
+            }
+
+            //playAgain = response.equalsIgnoreCase("Y");
+        }
+        System.out.println("Thanks for playing!");
         scanner.close();
     }
 }
